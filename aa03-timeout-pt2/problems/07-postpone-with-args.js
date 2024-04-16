39030f0490e796f382578f4c8585e1d274d733bd
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-04-16 00:32:10 -0400

    solved prob 7

--- aa03-timeout-pt2/problems/07-postpone-with-args.js
+++ aa03-timeout-pt2/problems/07-postpone-with-args.js
@@ -23,9 +23,19 @@ slowPrintSum(2, 8); // prints '10' after 500 ms
 ***********************************************************************/
 
 function postponeWithArgs(cb, delay) {
-  // Your code here 
+  return (...args) => setTimeout(() => cb(...args), delay)  
+  // return (...args) => setTimeout(cb, delay, ...args)  
 }
 
+const greet = (person) => console.log('Hello ' + person + '!');
+const slowGreet = postponeWithArgs(greet, 1000);
+slowGreet('Rose'); // prints 'Hello Rose!' after 1000 ms
+slowGreet('Alex'); // prints 'Hello Alex!' after 1000 ms
+
+const printSum = (num1, num2) => console.log(num1 + num2);
+const slowPrintSum = postponeWithArgs(printSum, 500);
+slowPrintSum(4, 3); // prints '7' after 500 ms
+slowPrintSum(2, 8); // prints '10' after 500 ms
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = postponeWithArgs;
